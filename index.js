dfae5c3ba7f2bc1b08fd05df85fecf783ed0a8ee
Jishnu <jishnu.nb@neoito.com>
2022-11-16 10:21:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const addTodo = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello addTodo`}))
+  res.end()
+  
+}
+
+export default addTodo
